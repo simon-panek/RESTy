0c5dd6e3379491d191db13d54d74b57fc07caf1c
Simon Panek <paneks19@gmail.com>
2021-01-12 16:29:59 -0800

    refactored day 26 after code review

--- resty/src/form.js
+++ resty/src/form.js
@@ -26,15 +26,27 @@ class Form extends React.Component {
 
   handleSubmit = e => {
     e.preventDefault();
+    // console.log('e ', e.target.url.value);
+    let radioSelection = e.target.routeType.value;
+    this.setState({ routeType: radioSelection });
+
+    let urlInput = e.target.url.value;
+    this.setState({ url: urlInput });
+
+    console.log({radioSelection}, {urlInput});
+    console.log('this.state.url ', this.state.url, 'this.state.routeType ', this.state.routeType);
+    console.log('this.state ', this.state);
+
     this.setState({ display: true });
+
   }
 
   render(){
     return(
       <>
-        <form>
+        {/* <form>
           {/* Put onSubmit on the form since that is where all of the info is, to capture = e.target.<name>.value*/}
-          <fieldset>
+          {/* <fieldset>
             <label>
               <input id="getRadio" type="radio" name="routeType" value="GET" onChange={this.handleRadio}/>
               GET
@@ -57,6 +69,34 @@ class Form extends React.Component {
                 <input id="urlInput" type='text' name="url" onChange={this.handleInput}/>
               </label>
               <button type="submit" onClick={this.handleSubmit}>Make it so.</button>
+            </section>
+          </fieldset>
+        </form> */}
+        <form onSubmit={this.handleSubmit}>
+          {/* Put onSubmit on the form since that is where all of the info is, to capture = e.target.<name>.value*/}
+          <fieldset>
+            <label>
+              <input id="getRadio" type="radio" name="routeType" value="GET" />
+              GET
+            </label>
+            <label>
+              <input id="postRadio" type="radio" name="routeType" value="POST" />
+              POST
+            </label>
+            <label>
+              <input id="putRadio" type="radio" name="routeType" value="PUT" />
+              PUT
+            </label>
+            <label>
+              <input id="deleteRadio" type="radio" name="routeType" value="DELETE" />
+              DELETE
+            </label>
+            <section id="buttonSection"> 
+              <label>
+                URL: 
+                <input id="urlInput" type='text' name="url" />
+              </label>
+              <button type="submit" >Make it so.</button>
             </section>
           </fieldset>
         </form>
