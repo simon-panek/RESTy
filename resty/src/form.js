0f953e64cf7366d5364a7be2a79199149904efed
Simon Panek <paneks19@gmail.com>
2021-01-12 16:51:39 -0800

    updated readme and uml

--- resty/src/form.js
+++ resty/src/form.js
@@ -12,21 +12,8 @@ class Form extends React.Component {
     }
   }
 
-  handleRadio = e => {
-    let radioSelection = e.target.value;
-    this.setState({ routeType: radioSelection });
-  }
-
-  handleInput = e => {
-    e.preventDefault();
-   
-    let urlInput = e.target.value;
-    this.setState({ url: urlInput });
-  }
-
   handleSubmit = e => {
     e.preventDefault();
-    // console.log('e ', e.target.url.value);
     let radioSelection = e.target.routeType.value;
     this.setState({ routeType: radioSelection });
 
@@ -44,36 +31,7 @@ class Form extends React.Component {
   render(){
     return(
       <>
-        {/* <form>
-          {/* Put onSubmit on the form since that is where all of the info is, to capture = e.target.<name>.value*/}
-          {/* <fieldset>
-            <label>
-              <input id="getRadio" type="radio" name="routeType" value="GET" onChange={this.handleRadio}/>
-              GET
-            </label>
-            <label>
-              <input id="postRadio" type="radio" name="routeType" value="POST" onChange={this.handleRadio}/>
-              POST
-            </label>
-            <label>
-              <input id="putRadio" type="radio" name="routeType" value="PUT" onChange={this.handleRadio}/>
-              PUT
-            </label>
-            <label>
-              <input id="deleteRadio" type="radio" name="routeType" value="DELETE" onChange={this.handleRadio}/>
-              DELETE
-            </label>
-            <section id="buttonSection"> 
-              <label>
-                URL: 
-                <input id="urlInput" type='text' name="url" onChange={this.handleInput}/>
-              </label>
-              <button type="submit" onClick={this.handleSubmit}>Make it so.</button>
-            </section>
-          </fieldset>
-        </form> */}
         <form onSubmit={this.handleSubmit}>
-          {/* Put onSubmit on the form since that is where all of the info is, to capture = e.target.<name>.value*/}
           <fieldset>
             <label>
               <input id="getRadio" type="radio" name="routeType" value="GET" />
