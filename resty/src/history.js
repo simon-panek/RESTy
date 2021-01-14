eb351f41c96069905211748829f68ba195539bd1
Simon Panek <paneks19@gmail.com>
2021-01-14 15:51:47 -0800

    reSearch buttons labeled and returning idx value

--- resty/src/history.js
+++ resty/src/history.js
@@ -3,16 +3,29 @@ import './history.scss';
 import ReactJson from 'react-json-view';
 
 
+
 class History extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      searchAgain : {}
+    }
+  }
+  
+  reRunHandler = (e) => {
+    let searchIdx = e.target.name;
+    console.log('INSIDE reRunHandler searchIdx ', searchIdx);
+  
+  }
 
   render(){
     
     return(
       <div id="historyDiv">
         <ul>
-        {this.props.searches.map(search => {
+        {this.props.searches.map((search, idx) => {
           return ( 
-          <li>{search.method} {search.url}</li> 
+          <button type="submit" name={idx} key={idx} onClick={this.reRunHandler}>{search.method} {search.url}</button> 
           )
             // make ths a link or a button so that it will send the method and url back to App.js and then send the new request
         })}
